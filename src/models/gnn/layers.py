1e5f720d368b3e4286d5dc61a6136c556e3ae83d
quyen228 <69098210+quyen228@users.noreply.github.com>
2023-04-09 17:43:21 +0700

    Update layers.py

--- src/models/gnn/layers.py
+++ src/models/gnn/layers.py
@@ -41,8 +41,8 @@ class Layer(object):
 
     def __init__(self, **kwargs):
         allowed_kwargs = {'name', 'logging'}
-        for kwarg in kwargs.keys():
-            assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
+        # for kwarg in kwargs.keys():
+        #     assert kwarg in allowed_kwargs, 'Invalid keyword argument: ' + kwarg
         name = kwargs.get('name')
         if not name:
             layer = self.__class__.__name__.lower()
